dcc27a29e1aac523a54259eb30b81e0fd3dc592d
Svante-J <72524651+Svante-J@users.noreply.github.com>
2021-10-06 17:20:23 +0200

    Add project files.

---
+++ Primtal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Primtal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("PLease enter number");
+
+            Console.WriteLine("_");
+
+            bool isPrime = true;
+
+            if (isPrime)
+            {
+                Console.WriteLine("wihoo");
+                myFuntion(5);
+            }
+
+
+        }
+
+        private static bool myFuntion(int input)
+        {
+            for (int i = 2; i < input; i++)
+            {
+                if (input % i ==0)
+                {
+                    return false;
+                }
+
+                // add to list
+            }
+            return true;
+        }
+    }
+}
