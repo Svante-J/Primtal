7a0233c242a85d29ecc31fc69112a4bd72aabceb
Svante-J <72524651+Svante-J@users.noreply.github.com>
2021-10-07 23:24:31 +0200

    1an fulad

--- Primtal/Program.cs
+++ Primtal/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 
-
 namespace Primtal
 {
-    internal class Program 
+    internal class Program
     {
         private const string inputMsg = ("enter postitive number");
         private static readonly string inputErrorMsg = ($" is not accepted\n {inputMsg}");
-        
 
         private static void Main(string[] args)
         {
@@ -15,25 +13,21 @@ namespace Primtal
             // var calculateMe = 6;
             while (true)
             {
-
                 Console.WriteLine(inputMsg);
                 var input = Console.ReadLine();
                 bool success = int.TryParse(input, out int result);
-                if (success && result > 0 )
+                if (success && result > 0)
                 {
-                    
-                        Console.WriteLine($"converted '{input}' to {result}");                        
-                    
+                    Console.WriteLine($"converted '{input}' to {result}");
                 }
                 else
                 {
                     Console.WriteLine($" '{input}'{inputErrorMsg}");
                     success = false;
-                    
                 }
 
                 // kollar parsing, att värdet inte är ett för att sen testa om det är ett primtal
-                if (success &&  result >= 2 && Primes.CalculatePrime(result))
+                if (success && result >= 2 && Primes.CalculatePrime(result))
                 {
                     Console.WriteLine($"{result} is a prime number");
                     PrimeList.Record(result);
@@ -45,17 +39,24 @@ namespace Primtal
                     Console.WriteLine($"{result} is not a prime");
                     Console.ReadLine();
                     isNotPrime = true;
-
                 }
-                if(isNotPrime)
+                if (isNotPrime)
                 {
-                var foundPrime = Primes.CalculateNextPrime(result);
-                PrimeList.Record(foundPrime);
-                Console.WriteLine($"but {foundPrime} is and have been added to the list!");
+                    var foundPrime = Primes.CalculateNextPrime(result);
+                    if (foundPrime == 1)
+                    { foundPrime++;
+                        PrimeList.Record(foundPrime);
+                        Console.WriteLine($"but {foundPrime} is and have been added to the list!");
+                    }
+                    else
+                    {
+                        PrimeList.Record(foundPrime);
+                        Console.WriteLine($"but {foundPrime} is and have been added to the list!");
+                    }
                 }
 
                 Console.WriteLine("här kommer listan");
-                
+
                 //todo: menyer
                 //söka nästa primtal
                 // PrimeList.Display();
