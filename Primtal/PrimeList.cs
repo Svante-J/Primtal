d350b1fdb5dd5f45047370e4ba9bb29e78187c16
Svante-J <72524651+Svante-J@users.noreply.github.com>
2021-10-12 13:11:01 +0200

    okok

--- Primtal/PrimeList.cs
+++ Primtal/PrimeList.cs
@@ -5,6 +5,7 @@ using System.Linq;
 
 namespace Primtal
 {
+    // Jag valde Lista som datastruktur då den hela tiden skall uppdateras/ändras
     internal static class PrimeList
     {
         private static List<int> _list;
